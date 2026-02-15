ede15c1473e45d9931c9c5437c124d7dd1ccb5c4
David Betancourt <ddavebet@gmail.com>
2026-02-15 18:20:21 -0500

    Agregar componente de inicio y configurar rutas para la gestión de transacciones

--- frontend/inventario-web-app/src/app/app.routes.ts
+++ frontend/inventario-web-app/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { TransaccionPageComponent } from './features/transacciones/pages/transaccion-page.component';
+import { InicioComponent } from './features/inicio/inicio.component';
 
 export const routes: Routes = [
   {
-    path: 'transacciones',
-    component: TransaccionPageComponent,
+    path: '',
+    component: InicioComponent,
+    children: [
+      { path: '', component: TransaccionPageComponent },
+      { path: 'transacciones', component: TransaccionPageComponent },
+    ],
   },
-  { path: '', redirectTo: 'transacciones', pathMatch: 'full' },
 ];
---
+++ frontend/inventario-web-app/src/app/features/inicio/inicio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-inicio',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './inicio.component.html',
+})
+export class InicioComponent {}
